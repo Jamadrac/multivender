a6e393ab9187c5570f46fdee371387c33023d36a
jamadrac <rabbikindalo001@@gmale.com>
2023-08-11 18:42:33 +0200

    sd

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from store.models import Product, Category
 
 def frontpage(request):
       #return httpresponse(dfghjklkjhgfd)
+
   #  products = Product.objects.filter(status=Product.ACTIVE)[0:6]
     products = Product.objects.all()
     category = Category.objects.all()
@@ -14,4 +15,4 @@ def frontpage(request):
 
 def about(request):
     #return httpresponse(dfghjklkjhgfd)
-    return render(request, 'core/about.html')
+    return render(request, 'core/about.html')                           
